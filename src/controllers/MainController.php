57d8ccc9491ff7c93124e86e289851b4667ea804
SentoMH <vimonher1986@gmail.com>
2023-12-20 20:29:21 +0100

    2.3 hecho

--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -3,22 +3,31 @@
 namespace AP\Controllers;
 
 use AP\Core\AbstractController;
+use AP\Core\EntityManager;
+use AP\Entity\PedidosEntity;
+use AP\Entity\LineasPedidosEntity;
 
 
 class MainController extends AbstractController
 {
+    private EntityManager $em;
+
+    public function __construct()
+    {
+        $this->em = new EntityManager();
+        parent::__construct();
+    }
+
     public function main(): void
     {
         $this->render("layout.html.twig", []);
     }
-
     /*    $method: Una cadena que representa el método o la acción que se ejecutó. Por ejemplo, podría ser "crearPedido" o "obtenerDatos".
     $msg: Un mensaje que se desea enviar en la respuesta. Por ejemplo, podría ser "Pedido creado con éxito" o "Error al procesar la solicitud".
     $status: Un código de estado HTTP. Los códigos de estado HTTP son estándar en la web y comunican el resultado de la solicitud (por ejemplo, 200 para éxito, 400 para solicitud incorrecta, 500 para error interno del servidor, etc.).*/
 
-    public function jsonResponse(?string $method, ?string $msg, ?int $status): void
+    public function jsonResponse( $method, $msg, $status)
     {
-
         if (is_null($status)) {
             $status = 400; //solicitud incorrecta si el status es null
         }
@@ -42,5 +51,52 @@ class MainController extends AbstractController
         $json = json_encode($arrayJson, http_response_code($status));
 
         echo $json;
+    }
+
+    public function crearPedidos(){
+
+        $entityM = $this->em->getEntityManager();
+        $pedidosRepository = $entityM->getRepository(PedidosEntity::class);
+        $lineasPedidoRepository = $entityM->getRepository(LineasPedidosEntity::class);
+        $data = [];
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method === 'POST') {
+            $data = [
+                'proveedor' => $_POST['idProveedor'],
+                'detalles' => $_POST['detalles'],
+                'productos' => $_POST['productos'],
+            ];
+        }
+        if (empty($data)) {
+            $msg = 'No se han recibido los parámetros correctos';
+            $this->jsonResponse($method, $msg, 400);
+        } else {
+            $pedido = $pedidosRepository->createPedido($data);
+            if (!$pedido) {
+                $msg = 'No se ha podido crear el pedido';
+                $this->jsonResponse("createPedido", $msg, 400);
+            } else {
+                $lineas = $lineasPedidoRepository->createLineaPedido($data['productos'], $pedido);
+                if (!$lineas) {
+                    $msg = 'No se ha podido crear el pedido';
+                    $this->jsonResponse("createLineaPedido", $msg, 400);
+                } else {
+                    $result = [];
+                    foreach ($lineas as $linea) {
+                        if (isset($linea)) {
+                            $result[] = $linea->jsonSerialize();
+                        }
+                    }
+                    $msg = [
+                        'message' => 'Se ha creado el pedido correctamente',
+                        'lineas' => $result,
+                    ];
+                    $this->jsonResponse(null, $msg, 200);
+
+                }
+            }
+        }
+
     }
 }
--- src/controllers/PedidosController.php
+++ src/controllers/PedidosController.php
@@ -2,59 +2,30 @@
 
 namespace AP\Controllers;
 
-use AP\Entity\PedidosEntity;
 use AP\Entity\ProductosEntity;
-use AP\Entity\ProveedoresEntity;
-use AP\Entity\LineasPedidosEntity;
 use AP\Core\AbstractController;
 use AP\Core\EntityManager;
-use AP\Controllers\MainController;
+
+
 
 class PedidosController extends AbstractController
 {
     private EntityManager $em;
-    private MainController $main;
 
     public function __construct()
     {
         $this->em = new EntityManager();
         parent::__construct();
-        $this->main = new MainController();
     }
 
     public function pedidos(): void
     {
         $entityM = $this->em->getEntityManager();
-        $pedidosRepository = $entityM->getRepository(PedidosEntity::class);
         $productosRepository = $entityM->getRepository(ProductosEntity::class);
-        $lineasPedidoRepository = $entityM->getRepository(LineasPedidosEntity::class);
         $productos = $productosRepository->findAll();
-        $mensajeExito = '';
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = [
-                'proveedor' => $_POST['idProveedor'],
-                'detalles' => $_POST['detalles'],
-                'productos' => $_POST['productos'],
-            ];
-
-            $pedido = $pedidosRepository->createPedido($data);
-
-            $lineas = $lineasPedidoRepository->createLineaPedido($data['productos'], $pedido);
-
-            
-            if ($pedido) {
-                $this->main->jsonResponse("crearPedido", "Pedido creado con éxito", 200);
-                return; 
-            } else {
-                $this->main->jsonResponse("crearPedido", "Error al crear el pedido", 500);
-                return; 
-            }
-        }
 
         $this->render("pedidos.html.twig", [
             'title' => 'Pedidos',
-            'mensajeExito' => $mensajeExito,
             'productos' => $productos
         ]);
     }
